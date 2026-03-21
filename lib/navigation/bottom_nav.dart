0e4904450036033490b64eb1f336da4c1c097c8e
Dignity <okorie.dignity@gmail.com>
2026-03-20 23:40:30 -0400

    Added the bottom navigation

---
+++ lib/navigation/bottom_nav.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import '../theme/app_theme.dart';
+import '../screens/home/home_screen.dart';
+import '../screens/library/library_screen.dart';
+import '../screens/tracker/tracker_screen.dart';
+import '../screens/progress/progress_screen.dart';
+import '../screens/ai_remix/ai_remix_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const LibraryScreen(),
+    const TrackerScreen(),
+    const ProgressScreen(),
+    const AIRemixScreen(),
+  ];
+
+  void _onNavItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onNavItemTapped,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: AppTheme.primaryDark,
+        selectedItemColor: AppTheme.accentGreen,
+        unselectedItemColor: AppTheme.textSecondary,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Track',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up),
+            label: 'Progress',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.smart_toy),
+            label: 'AI Remix',
+          ),
+        ],
+      ),
+    );
+  }
+}
